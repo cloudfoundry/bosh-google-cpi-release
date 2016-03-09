2d352686e3b35f62abcf32f48a4b1ae5eb29ee3a
Ferran Rodenas <frodenas@gmail.com>
2016-03-08 21:38:18 -0800

    Add CI promote candidate task

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -2,18 +2,18 @@
 
 set -e
 
+source bosh-google-cpi-boshrelease/ci/tasks/utils.sh
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
+cpi_release_name="bosh-google-cpi"
 semver=`cat version-semver/number`
 
-cd bosh-google-cpi-boshrelease
+pushd bosh-google-cpi-boshrelease
+  echo "Using BOSH CLI version..."
+  bosh version
 
-echo "Using BOSH CLI version..."
-bosh version
-
-cpi_release_name="bosh-google-cpi"
-
-echo "Building CPI BOSH Release..."
-bosh create release --name $cpi_release_name --version $semver --with-tarball
+  echo "Creating CPI BOSH Release..."
+  bosh create release --name $cpi_release_name --version $semver --with-tarball
+popd
 
 mv dev_releases/$cpi_release_name/$cpi_release_name-$semver.tgz ../candidate/
---
+++ ci/tasks/promote-candidate.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+source bosh-google-cpi-boshrelease/ci/tasks/utils.sh
+source /etc/profile.d/chruby-with-ruby-2.1.2.sh
+
+check_param aws_access_key_id
+check_param aws_secret_access_key
+
+# Creates an integer version number from the semantic version format
+# May be changed when we decide to fully use semantic versions for releases
+integer_version=`cut -d "." -f1 release-version-semver/number`
+echo $integer_version > promoted/integer_version
+
+cp -r bosh-google-cpi-boshrelease promoted/repo
+
+dev_release=$(echo $PWD/bosh-google-cpi-dev-artifacts/*.tgz)
+
+pushd promoted/repo
+  set +x
+  echo "Creating config/private.yml with blobstore secrets"
+  cat > config/private.yml << EOF
+---
+blobstore:
+  s3:
+    access_key_id: $aws_access_key_id
+    secret_access_key: $aws_secret_access_key
+EOF
+  set -x
+
+  echo "Using BOSH CLI version..."
+  bosh version
+
+  echo "Finalizing CPI BOSH Release..."
+  bosh finalize release ${dev_release} --version $integer_version
+
+  rm config/private.yml
+
+  git diff | cat
+  git add .
+
+  git config --global user.email cf-bosh-eng@pivotal.io
+  git config --global user.name CI
+  git commit -m "New Final CPI BOSH Release v$integer_version"
+popd
