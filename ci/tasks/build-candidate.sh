db6006ec5ea255a93efc7b9a4e5a0cff32e2c987
Ferran Rodenas <frodenas@gmail.com>
2016-03-09 00:31:33 -0800

    Fix CI build candidate task

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-source bosh-cpi-release/ci/tasks/utils.sh
+source bosh-cpi-src/ci/tasks/utils.sh
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
 cpi_release_name="bosh-google-cpi"
 semver=`cat version-semver/number`
 
-pushd bosh-cpi-release
+pushd bosh-cpi-src
   echo "Using BOSH CLI version..."
   bosh version
 
@@ -16,4 +16,4 @@ pushd bosh-cpi-release
   bosh create release --name ${cpi_release_name} --version ${semver} --with-tarball
 popd
 
-mv bosh-cpi-release/dev_releases/${cpi_release_name}/${cpi_release_name}-${semver}.tgz candidate/
+mv bosh-cpi-src/dev_releases/${cpi_release_name}/${cpi_release_name}-${semver}.tgz candidate/
