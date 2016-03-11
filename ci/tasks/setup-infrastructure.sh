e4e919b6b3672dcc19a18062299e4f5ee912b7b9
Ferran Rodenas <frodenas@gmail.com>
2016-03-11 13:08:14 -0800

    Fix some parms to set up & tear down google infrastructure

--- ci/tasks/setup-infrastructure.sh
+++ ci/tasks/setup-infrastructure.sh
@@ -6,9 +6,9 @@ source bosh-cpi-src/ci/tasks/utils.sh
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
 check_param google_project
-check_param google_default_zone
+check_param google_region
+check_param google_zone
 check_param google_json_key_data
-check_param google_network
 
 echo "Creating google json key..."
 mkdir -p $HOME/.config/gcloud/
@@ -17,10 +17,12 @@ echo "${google_json_key_data}" > $HOME/.config/gcloud/application_default_creden
 echo "Configuring google account..."
 gcloud auth activate-service-account --key-file $HOME/.config/gcloud/application_default_credentials.json
 gcloud config set project ${google_project}
-gcloud config set project ${google_default_zone}
+gcloud config set compute/region ${google_region}
+gcloud config set compute/zone ${google_zone}
 
 echo "Setting up google infrastructure..."
-gcloud compute addresses create bosh-ci-director
-gcloud compute networks create ${google_network} --mode auto
-gcloud compute firewall-rules create bosh-ci-intenal  --description "BOSH CI Internal traffic" --network ${google_network} --source-tags bosh-ci-intenal --target-tags bosh-ci-intenal --allow tcp,udp,icmp
-gcloud compute firewall-rules create bosh-ci-external --description "BOSH CI External traffic" --network ${google_network} --target-tags bosh-ci-external --allow tcp:22,tcp:443,tcp:4222,tcp:6868,tcp:25250,tcp:25555,tcp:25777,udp:53
+gcloud -q compute addresses create bosh-ci-director
+gcloud -q compute addresses create bosh-ci-bats
+gcloud -q compute networks create bosh-ci --mode auto
+gcloud -q compute firewall-rules create bosh-ci-intenal  --description "BOSH CI Internal traffic" --network ${google_network} --source-tags bosh-ci-intenal --target-tags bosh-ci-intenal --allow tcp,udp,icmp
+gcloud -q compute firewall-rules create bosh-ci-external --description "BOSH CI External traffic" --network ${google_network} --target-tags bosh-ci-external --allow tcp:22,tcp:443,tcp:4222,tcp:6868,tcp:25250,tcp:25555,tcp:25777,udp:53
--- ci/tasks/teardown-infrastructure.sh
+++ ci/tasks/teardown-infrastructure.sh
@@ -6,9 +6,9 @@ source bosh-cpi-src/ci/tasks/utils.sh
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
 check_param google_project
-check_param google_default_zone
+check_param google_region
+check_param google_zone
 check_param google_json_key_data
-check_param google_network
 
 echo "Creating google json key..."
 mkdir -p $HOME/.config/gcloud/
@@ -17,10 +17,14 @@ echo "${google_json_key_data}" > $HOME/.config/gcloud/application_default_creden
 echo "Configuring google account..."
 gcloud auth activate-service-account --key-file $HOME/.config/gcloud/application_default_credentials.json
 gcloud config set project ${google_project}
-gcloud config set project ${google_default_zone}
+gcloud config set compute/region ${google_region}
+gcloud config set compute/zone ${google_zone}
 
 echo "Tearing down google infrastructure..."
-gcloud compute firewall-rules delete bosh-ci-intenal
-gcloud compute firewall-rules delete bosh-ci-external
-gcloud compute networks delete ${google_network}
-gcloud compute addresses delete bosh-ci-director
+set +e
+gcloud -q compute firewall-rules delete bosh-ci-intenal
+gcloud -q compute firewall-rules delete bosh-ci-external
+gcloud -q compute networks delete bosh-ci
+gcloud -q compute addresses delete bosh-ci-director
+gcloud -q compute addresses delete bosh-ci-bats
+set -e
