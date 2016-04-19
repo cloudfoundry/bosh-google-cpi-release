df1b3efca3983bf0e494d09e3ddd135b79238026
Lyle Franklin <lylejfranklin@gmail.com>
2016-04-19 08:30:38 -0700

    Remove heavy stemcell building from stemcell pipeline (#1)
    * Remove "heavy" stemcell building from stemcell pipeline
    
    - As it stands today, the BOSH core team produces the "heavy"
      stemcells for each IaaS, meaning a stemcell containing a raw machine
      image.
    - This pipeline produces a "light" stemcell which contains a link to a
      machine image on GCS instead of bundling the image in the tarball.
    
    [#117185467](https://www.pivotaltracker.com/story/show/117185467)
    
    * Fix path typo in build-light-stemcell task
    
    - Also ensures environment variables are empty by default
    
    [#117185467](https://www.pivotaltracker.com/story/show/117185467)
    
    * Make build stemcell script executable
    
    [#117185467](https://www.pivotaltracker.com/story/show/117185467)
    
    * Ensure globbing expansion in stemcell path
    
    * Upload sha1 file in additional to stemcell assets
    
    - Allows for checksum to be displayed on bosh.io later on
    
    [#117185467](https://www.pivotaltracker.com/story/show/117185467)
    
    * Add temporary heavy stemcell bucket to light stemcell pipeline
    
    - The bucket will probably switch once we get closer to using the CPI in
      production
    
    [#116463189](https://www.pivotaltracker.com/story/show/116463189)

---
+++ ci/stemcell/tasks/build-light-stemcell.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+: ${BUCKET_NAME:?}
+
+# inputs
+stemcell_dir="$PWD/stemcell"
+
+# outputs
+light_stemcell_dir="$PWD/light-stemcell"
+raw_stemcell_dir="$PWD/raw-stemcell"
+
+echo "Creating light stemcell..."
+
+original_stemcell="$(echo ${stemcell_dir}/*.tgz)"
+original_stemcell_name="$(basename "${original_stemcell}")"
+raw_stemcell_name="$(basename "${original_stemcell}" .tgz)-raw.tar.gz"
+light_stemcell_name="light-${original_stemcell_name}"
+
+mkdir working_dir
+pushd working_dir
+  tar xvf "${original_stemcell}"
+
+  raw_stemcell_path="${raw_stemcell_dir}/${raw_stemcell_name}"
+  mv image "${raw_stemcell_path}"
+  echo -n $(sha1sum ${raw_stemcell_path} | awk '{print $1}') > ${raw_stemcell_path}.sha1
+
+  > image
+  echo "  source_url: https://storage.googleapis.com/${BUCKET_NAME}/${raw_stemcell_name}" >> stemcell.MF
+
+  light_stemcell_path="${light_stemcell_dir}/${light_stemcell_name}"
+  tar czvf "${light_stemcell_path}" *
+  echo -n $(sha1sum ${light_stemcell_path} | awk '{print $1}') > ${light_stemcell_path}.sha1
+popd
