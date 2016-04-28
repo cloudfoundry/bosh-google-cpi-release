46a114fa690c149500c527cee6f404311d2b649d
Evan Brown <evanbrown@google.com>
2016-04-28 16:14:37 -0700

    Separate network tests into own file

---
+++ src/bosh-google-cpi/integration/network_tests.go
@@ -0,0 +1,133 @@
+package integration
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Network", func() {
+	It("can create a VM with network IP forwarding enabled", func() {
+		request := fmt.Sprintf(`{
+			  "method": "create_vm",
+			  "arguments": [
+				"agent",
+				"%v",
+				{
+				  "machine_type": "n1-standard-1"
+				},
+				{
+				  "default": {
+					"type": "dynamic",
+					"cloud_properties": {
+					  "network_name": "%v",
+					  "ip_forwarding": true
+					}
+				  }
+				},
+				[],
+				{}
+			  ]
+			}`, existingStemcell, networkName)
+		vmCID = assertSucceedsWithResult(request).(string)
+		request = fmt.Sprintf(`{
+			  "method": "delete_vm",
+			  "arguments": ["%v"]
+			}`, vmCID)
+		assertSucceeds(request)
+	})
+
+	It("can create a VM with network tags", func() {
+		request := fmt.Sprintf(`{
+			  "method": "create_vm",
+			  "arguments": [
+				"agent",
+				"%v",
+				{
+				  "machine_type": "n1-standard-1"
+				},
+				{
+				  "default": {
+					"type": "dynamic",
+					"cloud_properties": {
+					  "network_name": "%v",
+					  "tags": ["tag1", "tag2"]
+					}
+				  }
+				},
+				[],
+				{}
+			  ]
+			}`, existingStemcell, networkName)
+		vmCID = assertSucceedsWithResult(request).(string)
+		request = fmt.Sprintf(`{
+			  "method": "delete_vm",
+			  "arguments": ["%v"]
+			}`, vmCID)
+		assertSucceeds(request)
+	})
+
+	It("can create a VM with an ephemeral external IP", func() {
+		request := fmt.Sprintf(`{
+			  "method": "create_vm",
+			  "arguments": [
+				"agent",
+				"%v",
+				{
+				  "machine_type": "n1-standard-1"
+				},
+				{
+				  "default": {
+					"type": "dynamic",
+					"cloud_properties": {
+					  "network_name": "%v",
+					  "ephemeral_external_ip": true
+					}
+				  }
+				},
+				[],
+				{}
+			  ]
+			}`, existingStemcell, networkName)
+		vmCID = assertSucceedsWithResult(request).(string)
+		request = fmt.Sprintf(`{
+			  "method": "delete_vm",
+			  "arguments": ["%v"]
+			}`, vmCID)
+		assertSucceeds(request)
+	})
+
+	It("can create a VM with a static external IP", func() {
+		request := fmt.Sprintf(`{
+			  "method": "create_vm",
+			  "arguments": [
+				"agent",
+				"%v",
+				{
+				  "machine_type": "n1-standard-1"
+				},
+				{
+				  "default": {
+					"type": "dynamic",
+					"cloud_properties": {
+					  "network_name": "%v",
+					  "ephemeral_external_ip": true
+					}
+				  },
+				  "vip": {
+					"type": "vip",
+					"ip": "%v"
+				  }  
+				},
+				[],
+				{}
+			  ]
+			}`, existingStemcell, networkName, externalStaticIP)
+		vmCID = assertSucceedsWithResult(request).(string)
+		request = fmt.Sprintf(`{
+			  "method": "delete_vm",
+			  "arguments": ["%v"]
+			}`, vmCID)
+		assertSucceeds(request)
+	})
+})
