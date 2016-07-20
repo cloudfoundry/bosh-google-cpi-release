dec6e48968b9f4e7887239dc50a19820e6cab896
Evan Brown <evandbrown@gmail.com>
2016-07-20 12:28:38 -0700

    ci: Automatically update docs with published CPI artifacts
    * Use sed to update README.md with details of promoted CPI

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -8,8 +8,13 @@ source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 check_param release_blobs_access_key
 check_param release_blobs_secret_key
 
-cpi_release_name="bosh-google-cpi"
+semver_version=`cat release-version-semver/number`
 integer_version=`cut -d "." -f1 release-version-semver/number`
+cpi_release_name="bosh-google-cpi"
+cpi_blob=${cpi_release_name}-${integer_version}.tgz 
+cpi_link=https://storage.googleapis.com/bosh-cpi-artifacts/bosh-google-cpi-$integer_version.tgz
+today=$(date +%Y-%m-%d)
+
 echo $integer_version > promoted/integer_version
 echo "BOSH Google CPI BOSH Release v${integer_version}" > promoted/annotation_message
 
@@ -36,12 +41,21 @@ EOF
 
   rm config/private.yml
 
+  # Insert CPI details into README.md
+  # Template markers in the README
+  cpi_marker="\[//\]: # (new-cpi)"
+  cpi_sha=$(sha1sum releases/$cpi_release_name/$cpi_blob | awk '{print $1}')
+  new_cpi="|$semver_version|$cpi_link|$cpi_sha|$today|"
+  sed -i "s^$cpi_marker^$new_cpi\n$cpi_marker^" README.md
+
   git diff | cat
   git add .
 
   git config --global user.email cf-bosh-eng@pivotal.io
   git config --global user.name CI
   git commit -m "BOSH Google CPI BOSH Release v${integer_version}"
+
+  mv releases/$cpi_release_name/$cpi_blob ../
+  echo $cpi_sha > ../$cpi_blob.sha1
 popd
 
-mv promoted/repo/releases/${cpi_release_name}/${cpi_release_name}-${integer_version}.tgz promoted/
