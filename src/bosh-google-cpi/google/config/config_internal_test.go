bdad5a5efb7a8b37458fb81664777b6fd0447c4c
Jeff Johnson <jrjohnson@google.com>
2017-05-12 12:40:57 -0400

    gofmt of e333f60df0

--- src/bosh-google-cpi/google/config/config_internal_test.go
+++ src/bosh-google-cpi/google/config/config_internal_test.go
@@ -1,44 +1,43 @@
 package config
 
-
 import (
-        . "github.com/onsi/ginkgo"
-        . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Config", func() {
-        var (
-                config Config
-        )
+	var (
+		config Config
+	)
 
 	Describe("UserAgent", func() {
-                It("returns correct user agent string with release, without prefix", func() {
-                        config.UserAgentPrefix = ""
-                        cpiRelease = "0.0.1"
+		It("returns correct user agent string with release, without prefix", func() {
+			config.UserAgentPrefix = ""
+			cpiRelease = "0.0.1"
 
-                        userAgent := config.GetUserAgent()
-                        Expect(userAgent).To(Equal("bosh-google-cpi/0.0.1"))
-                })
-                It("returns correct user agent string with release, with prefix", func() {
-                        config.UserAgentPrefix = "Kubo/0.0.2"
-                        cpiRelease = "0.0.1"
+			userAgent := config.GetUserAgent()
+			Expect(userAgent).To(Equal("bosh-google-cpi/0.0.1"))
+		})
+		It("returns correct user agent string with release, with prefix", func() {
+			config.UserAgentPrefix = "Kubo/0.0.2"
+			cpiRelease = "0.0.1"
 
-                        userAgent := config.GetUserAgent()
-                        Expect(userAgent).To(Equal("Kubo/0.0.2 bosh-google-cpi/0.0.1"))
-                })
-                It("returns correct user agent string without release, with prefix", func() {
-                        config.UserAgentPrefix = "Kubo/0.0.2"
-                        cpiRelease = ""
+			userAgent := config.GetUserAgent()
+			Expect(userAgent).To(Equal("Kubo/0.0.2 bosh-google-cpi/0.0.1"))
+		})
+		It("returns correct user agent string without release, with prefix", func() {
+			config.UserAgentPrefix = "Kubo/0.0.2"
+			cpiRelease = ""
 
-                        userAgent := config.GetUserAgent()
-                        Expect(userAgent).To(Equal("Kubo/0.0.2 bosh-google-cpi/dev"))
-                })
-                It("returns correct user agent string without release, without prefix", func() {
-                        config.UserAgentPrefix = ""
-                        cpiRelease = ""
+			userAgent := config.GetUserAgent()
+			Expect(userAgent).To(Equal("Kubo/0.0.2 bosh-google-cpi/dev"))
+		})
+		It("returns correct user agent string without release, without prefix", func() {
+			config.UserAgentPrefix = ""
+			cpiRelease = ""
 
-                        userAgent := config.GetUserAgent()
-                        Expect(userAgent).To(Equal("bosh-google-cpi/dev"))
-                })
-        })
+			userAgent := config.GetUserAgent()
+			Expect(userAgent).To(Equal("bosh-google-cpi/dev"))
+		})
+	})
 })
