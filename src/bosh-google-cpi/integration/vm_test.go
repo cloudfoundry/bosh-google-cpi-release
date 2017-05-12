7ce5aa9a6eb208fdd2d9bebda44466e4f787ce73
Jeff Johnson <jrjohnson@google.com>
2017-05-12 12:40:12 -0400

    Missing from bdd49

--- src/bosh-google-cpi/integration/vm_test.go
+++ src/bosh-google-cpi/integration/vm_test.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	computebeta "google.golang.org/api/compute/v0.beta"
+	compute "google.golang.org/api/compute/v1"
 )
 
 var _ = Describe("VM", func() {
