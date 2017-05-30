b2306647e951298d4c34acc6f2aa061f9a34849b
Zak Auerbach <zauerbach@pivotal.io>
2017-05-30 16:22:31 -0700

    Make sure to run tests on correct Go version (1.8.1)
    [#146126329](https://www.pivotaltracker.com/story/show/146126329)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- ci/tasks/unit-tests.sh
+++ ci/tasks/unit-tests.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+: ${GO_VERSION:?}
+
 export GOPATH=${PWD}/bosh-cpi-src
 export PATH=${GOPATH}/bin:$PATH
+
+current=$(go version)
+if [[ "$current" != *"$GO_VERSION"* ]]; then
+  echo "Go version is incorrect"
+  exit 1
+fi
 
 cd ${PWD}/bosh-cpi-src/src/bosh-google-cpi
 make test
