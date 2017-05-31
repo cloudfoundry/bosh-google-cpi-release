e38eebbc45ddf016b5af365d49b69134f030d59f
Chris Dutra <cdutra@pivotal.io>
2017-05-31 15:39:45 -0700

    Add function to check go version
    [#146126329](https://www.pivotaltracker.com/story/show/146126329)
    
    Signed-off-by: Zak Auerbach <zauerbach@pivotal.io>

--- ci/tasks/run-int.sh
+++ ci/tasks/run-int.sh
@@ -70,14 +70,7 @@ export ZONE=${google_zone}
 export GOPATH=${PWD}/bosh-cpi-src
 export PATH=${GOPATH}/bin:$PATH
 
-release_go_version="$(cat "$GOPATH/packages/golang/spec" | \
-  grep linux | awk '{print $2}' | sed "s/golang\/go\(.*\)\.linux-amd65.tar.gz/\1/")"
-
-current=$(go version)
-if [[ "$current" != *"$release_go_version"* ]]; then
-  echo "Go version is incorrect"
-  exit 1
-fi
+check_go_version $GOPATH
 
 cd ${PWD}/bosh-cpi-src/src/bosh-google-cpi
 env
--- ci/tasks/unit-tests.sh
+++ ci/tasks/unit-tests.sh
@@ -2,17 +2,12 @@
 
 set -e
 
+source bosh-cpi-src/ci/tasks/utils.sh
+
 export GOPATH=${PWD}/bosh-cpi-src
 export PATH=${GOPATH}/bin:$PATH
 
-release_go_version="$(cat "$GOPATH/packages/golang/spec" | \
-  grep linux | awk '{print $2}' | sed "s/golang\/go\(.*\)\.linux-amd65.tar.gz/\1/")"
-
-current=$(go version)
-if [[ "$current" != *"$release_go_version"* ]]; then
-  echo "Go version is incorrect"
-  exit 1
-fi
+check_go_version $GOPATH
 
 cd ${PWD}/bosh-cpi-src/src/bosh-google-cpi
 make test
