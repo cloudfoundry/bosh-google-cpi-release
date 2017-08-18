c875ca261ae29defa1537d4e984674a20794feff
Evan Brown <evanbrown@google.com>
2017-08-18 11:36:26 -0700

    ci: Get bosh-cli from resource
    The bosh-cli tool was being downloaded from a task script. This change
    ensures it is retrieved as a resource (and replaces the old bosh-init
    which was retrieved this way).

--- ci/tasks/teardown-director.sh
+++ ci/tasks/teardown-director.sh
@@ -8,19 +8,32 @@ source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 deployment_dir="${PWD}/deployment"
 google_json_key=${deployment_dir}/google_key.json
 manifest_filename="director-manifest.yml"
+certs=certs.yml
 
 echo "Creating google json key..."
 mkdir -p $HOME/.config/gcloud/
 cp ${google_json_key} $HOME/.config/gcloud/application_default_credentials.json
 
+# Export prefixed variables so they are accessible
+echo "Populating environment with BOSH_ prefixed vars"
+export BOSH_CONFIG=${deployment_dir}/.boshconfig
+export BOSH_director_username=$director_username
+export BOSH_director_password=$director_password
+export BOSH_cpi_release_name=$cpi_release_name
+export BOSH_google_zone=$google_zone
+export BOSH_google_project=$google_project
+export BOSH_google_address_static_director=$google_address_static_director
+export BOSH_director_ip=$director_ip
+export BOSH_google_test_bucket_name=$google_test_bucket_name
+export BOSH_google_network=$google_network
+export BOSH_google_subnetwork_gw=$google_subnetwork_gw
+export BOSH_google_subnetwork=$google_subnetwork
+export BOSH_google_subnetwork_range=$google_subnetwork_range
+export BOSH_google_firewall_internal=$google_firewall_internal
+export BOSH_google_firewall_external=$google_firewall_external
+export BOSH_google_json_key_data=$google_json_key_data
+
 pushd ${deployment_dir}
-  cp -r ./.bosh_init $HOME/
-
-  chmod +x ../bosh-init/bosh-init*
-
-  echo "Using bosh-init version..."
-  ../bosh-init/bosh-init* version
-
-  echo "Deleting BOSH Director..."
-  ../bosh-init/bosh-init* delete ${manifest_filename}
+  echo "Destroying BOSH Director..."
+  ./bosh delete-env -e micro-google --vars-store ${certs} --vars-env=BOSH ${manifest_filename}
 popd
