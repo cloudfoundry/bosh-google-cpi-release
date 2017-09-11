f6e14f04ea6472664fe4e6cd98447de66f4468a8
Chris Dutra <cdutra@pivotal.io>
2017-09-11 15:09:12 -0700

    Remove bosh.io token
    - print sha1

--- ci/stemcell/light/tasks/publish-stemcell-and-checksum.sh
+++ ci/stemcell/light/tasks/publish-stemcell-and-checksum.sh
@@ -18,18 +18,5 @@ light_stemcell_name="$(basename "${light_stemcell_path}")"
 echo "Uploading light stemcell ${light_stemcell_name} to ${OUTPUT_BUCKET}..."
 aws --endpoint-url=${AWS_ENDPOINT} s3 cp "${light_stemcell_path}" "s3://${OUTPUT_BUCKET}"
 
-if [ -n "${BOSHIO_TOKEN}" ]; then
-  echo "Publishing light stemcell checksum to bosh.io..."
-
-  checksum="$(sha1sum ${light_stemcell_dir}/*.tgz | awk '{print $1}')"
-
-  curl -X POST \
-    --fail \
-    -d "sha1=${checksum}" \
-    -H "Authorization: bearer ${BOSHIO_TOKEN}" \
-    "https://bosh.io/checksums/${light_stemcell_name}"
-
-  echo "Successfully published checksum!"
-else
-  echo "Checksum not provided, skipping publish checksum."
-fi
+checksum="$(sha1sum ${light_stemcell_dir}/*.tgz | awk '{print $1}')"
+echo "sha1: ${light_stemcell_name} ${checksum}"
