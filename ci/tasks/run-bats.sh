cloudfoundry/bosh-google-cpi-release

commit 6ce9486e3f4c063a39265f47228c741cd40f7e0b

ci: remove gcloud lookups for ip addresses
export the values directly with terraform instead

ci/tasks/run-bats.sh
@@ -15,7 +15,7 @@ check_param google_subnetwork_range
 check_param google_subnetwork_gw
 check_param google_firewall_internal
 check_param google_firewall_external
-check_param google_address_director
+check_param google_address_director_ip
 check_param google_address_bats
 check_param google_address_static_bats
 check_param google_address_static_pair_bats
@@ -45,7 +45,7 @@ echo "${private_key_data}" > ${private_key}
 export BAT_STEMCELL="${deployment_dir}/stemcell.tgz"
 export BAT_DEPLOYMENT_SPEC="${bat_config_filename}"
 export BAT_BOSH_CLI=/usr/bin/bosh2
-export BAT_DNS_HOST=${director_ip}
+export BAT_DNS_HOST=${google_address_director_ip}
 export BAT_INFRASTRUCTURE=google
 export BAT_NETWORKING=dynamic
 export BAT_PRIVATE_KEY=${private_key}
@@ -61,13 +61,8 @@ gcloud config set project ${google_project}
 gcloud config set compute/region ${google_region}
 gcloud config set compute/zone ${google_zone}
 
-echo "Looking for director IP..."
-director_ip=$(gcloud compute addresses describe ${google_address_director} --format json | jq -r '.address')
-export BAT_DIRECTOR=${director_ip}
-export BAT_DNS_HOST=${director_ip}
-
-echo "Looking for bats IP..."
-bats_ip=$(gcloud compute addresses describe ${google_address_bats} --format json | jq -r '.address')
+export BAT_DIRECTOR=${google_address_director_ip}
+export BAT_DNS_HOST=${google_address_director_ip}
 
 echo "Creating private key..."
 echo "${private_key_data}" > ${private_key}
@@ -97,7 +92,7 @@ properties:
     name: ${stemcell_name}
     version: latest
   instances: 1
-  vip: ${bats_ip}
+  vip: ${google_address_bats_ip}
   zone: ${google_zone}
   static_ips: [${google_address_static_pair_bats}]
   networks:

ci/infrastructure/outputs.tf
@@ -46,16 +46,16 @@ output "google_target_pool" {
   value = "${google_compute_target_pool.regional.name}"
 }
 
-output "google_address_director" {
-  value = "${google_compute_address.director.name}"
+output "google_address_director_ip" {
+  value = "${google_compute_address.director.address}"
+
+
+output "google_address_bats_ip" {
+  value = "${google_compute_address.bats.address}"
 }
 
-output "google_address_bats" {
-  value = "${google_compute_address.bats.name}"
-}
-
-output "google_address_int" {
-  value = "${google_compute_address.int.name}"
+output "google_address_int_ip" {
+  value = "${google_compute_address.int.address}"
 }
 
 output "google_service_account" {

ci/tasks/run-bats.yml
@@ -22,7 +22,7 @@ params:
   google_subnetwork_gw:                       replace-me
   google_firewall_internal:                   replace-me
   google_firewall_external:                   replace-me
-  google_address_director:                    replace-me
+  google_address_director_ip:                 replace-me
   google_address_bats:                        replace-me
   google_address_static_bats:                 replace-me
   google_address_static_pair_bats:            replace-me

ci/tasks/run-int.sh
@@ -34,6 +34,7 @@ export ZONE=${google_zone}
 export REGION=${google_region}
 export GOOGLE_PROJECT=${google_project}
 export SERVICE_ACCOUNT=${google_service_account}
+export EXTERNAL_STATIC_IP=${google_address_int_ip}
 export CPI_ASYNC_DELETE=true
 
 echo "Creating google json key..."
@@ -46,14 +47,6 @@ gcloud auth activate-service-account --key-file $HOME/.config/gcloud/application
 gcloud config set project ${google_project}
 gcloud config set compute/region ${google_region}
 gcloud config set compute/zone ${google_zone}
-
-# Find external IP
-echo "Looking for external IP..."
-external_ip=$(gcloud compute addresses describe ${google_address_int} --format json | jq -r '.address')
-export EXTERNAL_STATIC_IP=${external_ip}
-
-# Export zone
-export ZONE=${google_zone}
 
 # Setup Go and run tests
 export GOPATH=${PWD}/bosh-cpi-src

ci/tasks/setup-director.sh
@@ -42,9 +42,6 @@ gcloud config set project ${google_project}
 gcloud config set compute/region ${google_region}
 gcloud config set compute/zone ${google_zone}
 
-echo "Looking for director IP..."
-director_ip=$(gcloud compute addresses describe ${google_address_director} --format json | jq -r '.address')
-
 echo "Creating private key..."
 echo "${private_key_data}" > ${private_key}
 chmod go-r ${private_key}
@@ -127,7 +124,7 @@ pushd ${deployment_dir}
       -v internal_cidr=${google_subnetwork_range} \
       -v internal_gw=${google_subnetwork_gw} \
       -v internal_ip=${google_address_static_director} \
-      -v external_ip=${director_ip} \
+      -v external_ip=${google_address_director_ip} \
       --var-file gcp_credentials_json=${google_json_key} \
       -v project_id=${google_project} \
       -v zone=${google_zone} \

ci/tasks/teardown-director.sh
@@ -35,7 +35,7 @@ pushd ${deployment_dir}
       -v internal_cidr=${google_subnetwork_range} \
       -v internal_gw=${google_subnetwork_gw} \
       -v internal_ip=${google_address_static_director} \
-      -v external_ip=${director_ip} \
+      -v external_ip=${google_address_director_ip} \
       --var-file gcp_credentials_json=${google_json_key} \
       -v project_id=${google_project} \
       -v zone=${google_zone} \

ci/tasks/utils.sh
@@ -69,8 +69,8 @@ function read_infrastructure {
   export google_backend_service=$(cat ${infrastructure_metadata} | jq -r .google_backend_service)
   export google_region_backend_service=$(cat ${infrastructure_metadata} | jq -r .google_region_backend_service)
   export google_target_pool=$(cat ${infrastructure_metadata} | jq -r .google_target_pool)
-  export google_address_director=$(cat ${infrastructure_metadata} | jq -r .google_address_director)
-  export google_address_bats=$(cat ${infrastructure_metadata} | jq -r .google_address_bats)
-  export google_address_int=$(cat ${infrastructure_metadata} | jq -r .google_address_int)
+  export google_address_director_ip=$(cat ${infrastructure_metadata} | jq -r .google_address_director_ip)
+  export google_address_bats_ip=$(cat ${infrastructure_metadata} | jq -r .google_address_bats_ip)
+  export google_address_int_ip=$(cat ${infrastructure_metadata} | jq -r .google_address_int_ip)
   export google_service_account=$(cat ${infrastructure_metadata} | jq -r .google_service_account)
 }

commit 5b1b42b6cecf4527f8e721b280b5394946a3337c

ci: use proper variable for the director address
broken in 6ce9486e3f

ci/tasks/run-bats.sh
@@ -74,7 +74,7 @@ echo "Using BOSH CLI version..."
 ${BAT_BOSH_CLI} --version
 
 echo "Setting up BOSH v2..."
-export BOSH_ENVIRONMENT="${director_ip}"
+export BOSH_ENVIRONMENT="${google_address_director_ip}"
 export BOSH_CLIENT="admin"
 export BOSH_CLIENT_SECRET="=$(${BAT_BOSH_CLI} int ${creds_file} --path /admin_password)"
 export BOSH_CA_CERT="$(${BAT_BOSH_CLI} interpolate ${creds_file} --path /director_ssl/ca)"