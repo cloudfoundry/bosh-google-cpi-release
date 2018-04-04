05630e2ecdef2b7764a6728e7b73b2700b8ee84d
Jeff Johnson <jrjohnson@google.com>
2018-04-04 14:16:52 -0700

    ci/build-candidate: use bosh2

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -7,6 +7,7 @@ source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
 cpi_release_name="bosh-google-cpi"
 semver=`cat version-semver/number`
+image_path=$PWD/bosh-cpi-src/${cpi_release_name}-${semver}.tgz
 
 pushd bosh-cpi-src
   echo "Using BOSH CLI version..."
@@ -17,10 +18,9 @@ pushd bosh-cpi-src
 
   # We have to use the --force flag because we just added the `src/bosh-google-cpi/release` file
   echo "Creating CPI BOSH Release..."
-  bosh create release --name ${cpi_release_name} --version ${semver} --with-tarball --force
+  bosh2 create-release --name=${cpi_release_name} --version=${semver} --tarball=${image_path} --force
 popd
 
-image_path=bosh-cpi-src/dev_releases/${cpi_release_name}/${cpi_release_name}-${semver}.tgz
 echo -n $(sha1sum $image_path | awk '{print $1}') > $image_path.sha1
 
 mv ${image_path} candidate/
