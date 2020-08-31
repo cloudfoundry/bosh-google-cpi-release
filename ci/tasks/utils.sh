28a3dc6889cfb5fcf8887622770dda67bd147e07
Ruben Koster <rkoster@starkandwayne.com>
2020-08-31 15:30:41 +0200

    Switch to bosh-packages golang + bump to version 1.14 [#174536032]

--- ci/tasks/utils.sh
+++ ci/tasks/utils.sh
@@ -45,8 +45,8 @@ function add_on_exit {
 
 function check_go_version {
   local cpi_release=$1
-  local release_go_version="$(cat "$cpi_release/packages/golang/spec" | \
-    grep linux | awk '{print $2}' | sed "s/golang\/go\(.*\)\.linux-amd64.tar.gz/\1/")"
+  local release_go_version="$(cat $cpi_release/packages/golang-*/spec.lock | \
+    grep linux | awk '{print $2}' | sed "s/golang-\(.*\)-linux/\1/")"
 
   local current=$(go version)
   if [[ "$current" != *"$release_go_version"* ]]; then
