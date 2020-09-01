b810fd40e6c71d4c1086049c909d5681d7cd81ca
Ruben Koster <rkoster@starkandwayne.com>
2020-09-01 12:36:38 +0200

    Update to terraform 0.12.x to align with used concourse resource [#174536032]

--- ci/test_infrastructure/provider.tf
+++ ci/test_infrastructure/provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  version = "v1.5.0"
+  version = "~> 2.5"
 
-  project     = "${var.google_project}"
-  region      = "${var.google_region}"
-  credentials = "${var.google_json_key_data}"
+  project     = var.google_project
+  region      = var.google_region
+  credentials = var.google_json_key_data
 }
 
 provider "random" {
-  version = "~> 1.1.0"
+  version = "~> 2.1"
 }
--- ci/test_infrastructure/resources.tf
+++ ci/test_infrastructure/resources.tf
@@ -17,7 +17,7 @@ resource "google_compute_address" "director" {
 resource "google_compute_address" "director_internal" {
   name         = "${var.prefix}-dir-internal"
   address_type = "INTERNAL"
-  subnetwork   = "${google_compute_subnetwork.subnetwork.self_link}"
+  subnetwork   = google_compute_subnetwork.subnetwork.self_link
 }
 
 resource "google_compute_address" "bats" {
@@ -32,12 +32,12 @@ resource "google_compute_address" "int_internal" {
   count        = 3
   name         = "${var.prefix}-int-internal-${count.index}"
   address_type = "INTERNAL"
-  subnetwork   = "${google_compute_subnetwork.subnetwork.self_link}"
+  subnetwork   = google_compute_subnetwork.subnetwork.self_link
 }
 
 resource "google_compute_network" "auto" {
-   name                    = "${var.prefix}-auto"
-   auto_create_subnetworks = true
+  name                    = "${var.prefix}-auto"
+  auto_create_subnetworks = true
 }
 
 resource "google_compute_network" "manual" {
@@ -46,17 +46,17 @@ resource "google_compute_network" "manual" {
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name          = "${var.prefix}"
-  ip_cidr_range = "${var.google_subnetwork_range}"
-  network       = "${google_compute_network.manual.self_link}"
+  name          = var.prefix
+  ip_cidr_range = var.google_subnetwork_range
+  network       = google_compute_network.manual.self_link
 }
 
 resource "google_compute_firewall" "internal" {
   name        = "${var.prefix}-int"
   description = "BOSH CI Internal Traffic"
-  network     = "${google_compute_network.manual.self_link}"
-  source_tags = ["${var.google_firewall_internal}"]
-  target_tags = ["${var.google_firewall_internal}"]
+  network     = google_compute_network.manual.self_link
+  source_tags = [var.google_firewall_internal]
+  target_tags = [var.google_firewall_internal]
 
   allow {
     protocol = "tcp"
@@ -74,8 +74,8 @@ resource "google_compute_firewall" "internal" {
 resource "google_compute_firewall" "external" {
   name        = "${var.prefix}-ext"
   description = "BOSH CI External Traffic"
-  network     = "${google_compute_network.manual.self_link}"
-  target_tags = ["${var.google_firewall_external}"]
+  network     = google_compute_network.manual.self_link
+  target_tags = [var.google_firewall_external]
 
   allow {
     protocol = "tcp"
@@ -95,27 +95,27 @@ resource "google_compute_firewall" "external" {
 # Target Pool
 resource "google_compute_target_pool" "regional" {
   name   = "${var.prefix}-r"
-  region = "${var.google_region}"
+  region = var.google_region
 }
 
 # Backend Service
 resource "google_compute_instance_group" "backend_service" {
-  name = "${var.prefix}"
-  zone = "${var.google_zone}"
+  name = var.prefix
+  zone = var.google_zone
 }
 
 resource "google_compute_http_health_check" "backend_service" {
-  name = "${var.prefix}"
+  name = var.prefix
 }
 
 resource "google_compute_backend_service" "backend_service" {
-  health_checks = ["${google_compute_http_health_check.backend_service.self_link}"]
-  name          = "${var.prefix}"
+  health_checks = [google_compute_http_health_check.backend_service.self_link]
+  name          = var.prefix
   port_name     = "http"
   timeout_sec   = "30"
 
   backend {
-    group           = "${google_compute_instance_group.backend_service.self_link}"
+    group           = google_compute_instance_group.backend_service.self_link
     balancing_mode  = "UTILIZATION"
     capacity_scaler = "1"
     max_utilization = "0.8"
@@ -132,33 +132,31 @@ resource "google_compute_health_check" "region_backend_service" {
 }
 
 resource "google_compute_instance_group" "region_backend_service" {
-  name      = "${var.prefix}-r"
-  zone      = "${var.google_zone}"
-  network       = "${google_compute_network.manual.self_link}"
+  name    = "${var.prefix}-r"
+  zone    = var.google_zone
+  network = google_compute_network.manual.self_link
 }
-
 
 resource "google_compute_region_backend_service" "region_backend_service" {
   name          = "${var.prefix}-r"
-  health_checks = ["${google_compute_health_check.region_backend_service.self_link}"]
-  region        = "${var.google_region}"
+  health_checks = [google_compute_health_check.region_backend_service.self_link]
+  region        = var.google_region
   protocol      = "TCP"
   timeout_sec   = "30"
 
   backend {
-    group = "${google_compute_instance_group.region_backend_service.self_link}"
+    group = google_compute_instance_group.region_backend_service.self_link
   }
 }
 
-
 resource "google_compute_backend_service" "collision_backend_service" {
-  health_checks = ["${google_compute_http_health_check.backend_service.self_link}"]
+  health_checks = [google_compute_http_health_check.backend_service.self_link]
   name          = "${var.prefix}-collision"
   port_name     = "http"
   timeout_sec   = "30"
 
   backend {
-    group           = "${google_compute_instance_group.backend_service.self_link}"
+    group           = google_compute_instance_group.backend_service.self_link
     balancing_mode  = "UTILIZATION"
     capacity_scaler = "1"
     max_utilization = "0.8"
@@ -167,12 +165,13 @@ resource "google_compute_backend_service" "collision_backend_service" {
 
 resource "google_compute_region_backend_service" "collision_region_backend_service" {
   name          = "${var.prefix}-collision"
-  health_checks = ["${google_compute_health_check.region_backend_service.self_link}"]
-  region        = "${var.google_region}"
+  health_checks = [google_compute_health_check.region_backend_service.self_link]
+  region        = var.google_region
   protocol      = "TCP"
   timeout_sec   = "30"
 
   backend {
-    group = "${google_compute_instance_group.region_backend_service.self_link}"
+    group = google_compute_instance_group.region_backend_service.self_link
   }
 }
+
