08670fec30d957d46f49a9c31cdfea17fd2f1cf3
Maya Rosecrance <mrosecrance@pivotal.io>
2020-09-30 13:06:06 -0700

    Remove cpi publishing
    - bosh io will already publish it and this release doesn't need to host
    it's own copy
    - there was a bunch of stuff to track versions/stemcells in the readme
    as well which was ripped out  a long time ago.`
    
    [#175053360](https://www.pivotaltracker.com/story/show/175053360)

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -12,22 +12,7 @@ semver_version=`cat release-version-semver/number`
 echo $semver_version > promoted/semver_version
 echo "BOSH Google CPI BOSH Release v${semver_version}" > promoted/annotation_message
 
-today=$(date +%Y-%m-%d)
 cp -r bosh-cpi-src promoted/repo
-
-# CPI vars
-cpi_release_name="bosh-google-cpi"
-cpi_blob=${cpi_release_name}-${semver_version}.tgz
-cpi_link=https://storage.googleapis.com/bosh-cpi-artifacts/bosh-google-cpi-$semver_version.tgz
-
-# Stemcell vars
-stemcell_path=$(basename `ls stemcell/*.tgz`)
-stemcell_name=${stemcell_path%.*}
-stemcell_version=`cat stemcell/version`
-stemcell_url=`cat stemcell/url | sed "s|gs://|https://storage.googleapis.com/|"`
-stemcell_type=Heavy
-if [[ $stemcell_name == light* ]]; then stemcell_type=Light; fi
-stemcell_sha=$(sha1sum stemcell/*.tgz | awk '{print $1}')
 
 dev_release=$(echo $PWD/bosh-cpi-release/*.tgz)
 
@@ -50,20 +35,10 @@ EOF
 
   rm config/private.yml
 
-  # Insert CPI details into README.md
-  # Template markers in the README
-  cpi_marker="\[//\]: # (new-cpi)"
-  cpi_sha=$(sha1sum releases/$cpi_release_name/$cpi_blob | awk '{print $1}')
-  new_cpi="|[$semver_version]($cpi_link)|$cpi_sha|$today|"
-  sed -i "s^$cpi_marker^$new_cpi\n$cpi_marker^" README.md
-
   git diff | cat
   git add .
 
   git config --global user.email cf-bosh-eng@pivotal.io
   git config --global user.name CI
   git commit -m "BOSH Google CPI BOSH Release v${semver_version}"
-
-  mv releases/$cpi_release_name/$cpi_blob ../
-  echo $cpi_sha > ../$cpi_blob.sha1
 popd
