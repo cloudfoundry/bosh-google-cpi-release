3042c47572623ea97aeff572a336e7bec47e6184
Joseph Palermo <jpalermo@pivotal.io>
2023-01-12 17:38:23 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,13 +4,6 @@ set -eu
 
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
-until lpass status;do
-    LPASS_DISABLE_PINENTRY=1 lpass ls a
-    sleep 1
-done
-
 fly -t bosh-ecosystem set-pipeline \
     -p bosh-google-cpi \
-    -c ${script_dir}/pipeline.yml \
-    -v dockerhub_password=$(lpass show "Docker Hub" --password) \
-    -l <(lpass show --notes "google-cpi concourse secrets")
+    -c ${script_dir}/pipeline.yml
