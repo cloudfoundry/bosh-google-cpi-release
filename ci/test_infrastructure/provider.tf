fc34e512b8b5192125d73388b9d3a321443fe7bb
Long Nguyen <nguyenlo@vmware.com>
2023-02-28 10:21:02 -0500

    Removing constraints

--- ci/test_infrastructure/provider.tf
+++ ci/test_infrastructure/provider.tf
@@ -1,11 +1,8 @@
 provider "google" {
-  version = "~> 2.5"
-
   project     = var.google_project
   region      = var.google_region
   credentials = var.google_json_key_data
 }
 
 provider "random" {
-  version = "~> 2.1"
 }
